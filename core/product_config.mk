983a2f78403c226d536a0e20ce0c61376364594c
AKabhishek <abhishekkaushik.star@gmail.com>
2017-02-18 11:01:57 +0000

    DarkNess reDefined Bringup
    - With ascii art on package completion
    
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- core/product_config.mk
+++ core/product_config.mk
@@ -179,13 +179,9 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# A CM build needs only the CM product makefiles.
-ifneq ($(CM_BUILD),)
-  all_product_configs := $(shell find device -path "*/$(CM_BUILD)/lineage.mk")
-  ifeq ($(all_product_configs),)
-    # Fall back to cm.mk
-    all_product_configs := $(shell find device -path "*/$(CM_BUILD)/cm.mk")
-  endif
+# A DND build needs only the DND product makefiles.
+ifneq ($(DND_BUILD),)
+  all_product_configs := $(shell find device -path "*/$(DND_BUILD)/dnd.mk")
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
@@ -196,9 +192,9 @@ else
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
   endif # TARGET_BUILD_APPS
-endif # CM_BUILD
+endif # DND_BUILD
 
-ifeq ($(CM_BUILD),)
+ifeq ($(DND_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
